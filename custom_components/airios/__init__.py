38d94fa6706c37da1136c93c817afaf9e83307c5
Samuel Cabrero <samuel@orica.es>
2025-03-08 16:51:39 +0100

    airios: A integration for ventilation systems
    Airios is an OEM that develop and produce parts for for residential
    ventilation systems from controllers to remote controls and sensors.
    
    All of these communicate over RAMSES-II protocol from Honeywell so the
    integration requires a RF bridge. Currently only the RS485 serial model
    is supported.
    
    Some manufacturers known to build ventilation systems based on Airios
    controllers are:
    
    * Siber (fully tested while development)
    
    Also, this integration would be potentially compatible with:
    
    * Alnor PremAIR, SlimAIR or MinistAIR series
    * Vasco
    * Meltem
    * Mitsubishi
    * Hopper
    * Nuaire
    
    An probably others like:
    * Itho
    * Orcon
    * Evohome
    * Sundial
    * Hometronics
    
    (list taken from https://github.com/zxdavb/ramses_rf)
    
    Signed-off-by: Samuel Cabrero <samuel@orica.es>

---
+++ custom_components/airios/__init__.py
@@ -0,0 +1,87 @@
+"""The Airios integration."""
+
+from __future__ import annotations
+
+import logging
+import typing
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_ADDRESS,
+    CONF_DEVICE,
+    CONF_HOST,
+    CONF_PORT,
+    CONF_TYPE,
+    Platform,
+)
+from homeassistant.exceptions import ConfigEntryError, ConfigEntryNotReady
+from pyairios import Airios, AiriosRtuTransport, AiriosTcpTransport
+
+from .const import DEFAULT_SCAN_INTERVAL, BridgeType
+from .coordinator import AiriosDataUpdateCoordinator
+
+if typing.TYPE_CHECKING:
+    from homeassistant.core import HomeAssistant
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS: list[Platform] = [
+    Platform.FAN,
+]
+
+type AiriosConfigEntry = ConfigEntry[AiriosDataUpdateCoordinator]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: AiriosConfigEntry) -> bool:
+    """Set up Airios from a config entry."""
+    bridge_type = entry.data[CONF_TYPE]
+    if bridge_type == BridgeType.SERIAL:
+        device = entry.data[CONF_DEVICE]
+        transport = AiriosRtuTransport(device)
+    elif bridge_type == BridgeType.NETWORK:
+        host = entry.data[CONF_HOST]
+        port = entry.data[CONF_PORT]
+        transport = AiriosTcpTransport(host, port)
+    else:
+        msg = f"Unexpected bridge type {bridge_type}"
+        raise ConfigEntryError(msg)
+
+    modbus_address = entry.data[CONF_ADDRESS]
+    api = Airios(transport, modbus_address)
+
+    update_interval = DEFAULT_SCAN_INTERVAL
+    coordinator = AiriosDataUpdateCoordinator(hass, api, update_interval)
+    await coordinator.async_config_entry_first_refresh()
+
+    bridge_rf_address = await api.bridge.node_rf_address()
+    if bridge_rf_address is None or bridge_rf_address.value is None:
+        msg = "Failed to get bridge RF address"
+        raise ConfigEntryNotReady(msg)
+    bridge_rf_address = bridge_rf_address.value
+
+    if entry.unique_id != str(bridge_rf_address):
+        message = (
+            f"Unexpected device {bridge_rf_address} found, expected {entry.unique_id}"
+        )
+        _LOGGER.error(message)
+        raise ConfigEntryNotReady(message)
+
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+    entry.runtime_data = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def update_listener(hass: HomeAssistant, entry: AiriosConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: AiriosConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        coordinator: AiriosDataUpdateCoordinator = entry.runtime_data
+        coordinator.api.close()
+    return unload_ok
