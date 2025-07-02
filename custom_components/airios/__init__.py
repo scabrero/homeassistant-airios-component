00b6d6d4f4bff21fd42f0293f40043b2d6be21df
Samuel Cabrero <samuel@orica.es>
2025-07-02 13:55:27 +0200

    airios: Sensor platform for Airios integration
    Signed-off-by: Samuel Cabrero <samuel@orica.es>

--- custom_components/airios/__init__.py
+++ custom_components/airios/__init__.py
@@ -30,6 +30,7 @@ _LOGGER = logging.getLogger(__name__)
 PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
     Platform.FAN,
+    Platform.SENSOR,
 ]
 
 type AiriosConfigEntry = ConfigEntry[AiriosDataUpdateCoordinator]
